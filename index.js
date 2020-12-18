a8ca1935e6bbf6ec9b05f8c99f2c551bf2f3d109
Philipp Steckenbauer <42046147+philipp122@users.noreply.github.com>
2020-12-18 20:55:46 +0100

    Extracted Configuration in separated Class; Version 1.0.0-beta.6

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Datastore = require('./lib/Datastore.class');
 const Adapter = require('./lib/Adapter/Adapter.class');
 const MemoryAdapter = require('./lib/Adapter/Memory.Adapter.class');
 const FileAdapter = require('./lib/Adapter/File.Adapter.class');
+const Config = require('./lib/Util/Config.class');
 
 module.exports = {
 	DataStore: Datastore,
-	Adapter: {
+	Util: {
 		Adapter: Adapter,
-		Memory: MemoryAdapter,
-		File: FileAdapter
+		MemoryAdapter: MemoryAdapter,
+		FileAdapter: FileAdapter,
+		Config: Config
 	}
 };
