4af57d04087c75e2716b366c381b801af94d6bbc
Philipp Steckenbauer <42046147+philipp122@users.noreply.github.com>
2020-12-18 18:30:52 +0100

    Updated exports

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const Datastore = require('./lib/Datastore.class');
+const Adapter = require('./lib/Adapter/Adapter.class');
+const MemoryAdapter = require('./lib/Adapter/Adapter.class');
+const FileAdapter = require('./lib/Adapter/Adapter.class');
 
-module.exports = Datastore;
+module.exports = {
+	DataStore: Datastore,
+	Adapter: {
+		Adapter: Adapter,
+		Memory: MemoryAdapter,
+		File: FileAdapter
+	}
+};
