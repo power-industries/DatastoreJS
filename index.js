a873e35bd2eef3034e4b1f2d51920d26484f995b
Philipp Steckenbauer <42046147+philipp122@users.noreply.github.com>
2020-12-30 17:16:36 +0100

    Major rewrite

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const Datastore = require('./lib/Datastore.class');
-const Adapter = require('./lib/Adapter/Adapter.class');
-const MemoryAdapter = require('./lib/Adapter/Memory.Adapter.class');
-const FileAdapter = require('./lib/Adapter/File.Adapter.class');
-const Config = require('./lib/Util/Config.class');
-const Schema = require('@power-industries/schemajs');
-
-module.exports = {
-	DataStore: Datastore,
-	Schema: Schema,
-	Util: {
-		Adapter: Adapter,
-		MemoryAdapter: MemoryAdapter,
-		FileAdapter: FileAdapter,
-		Config: Config
-	}
-};
+module.exports = require('./lib/Datastore.class');
